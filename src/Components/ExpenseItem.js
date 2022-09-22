8151fdf6ed6587b434f5783e34c78346fee4bd76
shubhamgpta7 <shubhamgpta7@gmail.com>
2022-09-22 11:41:32 +0545

    Added Custom Wrapping Component -Card

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
@@ -5,7 +6,7 @@ import './ExpenseItem.css'
 function ExpenseItem(props){
     
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <div>
                 <ExpenseDate date={props.date}/>
             </div>
@@ -13,7 +14,7 @@ function ExpenseItem(props){
                 <h2>{props.title }</h2>
                 <div className='expense-item__price'>${ props.amount }</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
