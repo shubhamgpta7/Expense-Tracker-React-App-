d2459bef4da694bf3bfbcdd0b324c39e8c3b7d3a
shubhamgpta7 <shubhamgpta7@gmail.com>
2022-09-24 15:11:49 +0545

    Learning useState in React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./Components/Expenses/Expenses";
+import NewExpense from "./Components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -26,6 +27,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense/>
       <Expenses expenses={expenses}/>
     </div>
   );
